1a2b3a8d8986af7e4f4d786ffb918f84a32750a0
huxingkong <huxingkong@coinmex.com>
2019-08-06 23:09:01 +0800

    add https method

--- src/main/java/com/marke/marketmaker/utils/Test.java
+++ src/main/java/com/marke/marketmaker/utils/Test.java
@@ -1,9 +1,24 @@
 package com.marke.marketmaker.utils;
 
+import org.jsoup.Connection;
+
+import java.io.IOException;
+
 public class Test {
 
     public static void main(String[] args) {
-       String result =  HttpUtil.doPost("https://api.huobi.br.com/market/detail?symbol=ethusdt");
+
+//        String param = "{\"symbol\":\"ethusdt\",\"size\":2}";
+//        String param = "{}";
+//       String result =  HttpUtil.doPost("https://api.huobi.br.com/market/history/trade?symbol=ethusdt&size=2",param,"utf-8");
+//        System.out.println(result);
+        try {
+           Connection.Response response =  HttpUtil.get("https://api.huobi.br.com/market/history/trade?symbol=ethusdt&size=2");
+            System.out.println(response.body());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
